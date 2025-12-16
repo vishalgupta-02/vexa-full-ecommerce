0f8a7f7257d36c1195387768c624e190cf3fb6fe
CodeMonkey <abhimanyug987@gmail.com>
2025-12-17 02:03:24 +0530

    turborepo setup & few services created

---
+++ apps/product-service/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Product Service is running on port ${PORT}`);
+});
